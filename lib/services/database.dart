36969406ce9fe94a05fe7964215518c52cc8f0f2
--author= <hanimohammedhmk@gmail.com>
2022-01-29 13:16:12 +0530

    commit

---
+++ lib/services/database.dart
@@ -0,0 +1,31 @@
+import 'package:brew_crew/models/brew.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatabaseService {
+  final String? uid;
+  DatabaseService({this.uid});
+  //collection reference
+  final CollectionReference brewCollection =
+      FirebaseFirestore.instance.collection('brews');
+
+  Future updateUserData(String sugar, String name, int strength) async {
+    return await brewCollection
+        .doc(uid)
+        .set({'sugar': sugar, 'name': name, 'strength': strength});
+  }
+
+  //brew list from snapshot
+  List<Brew> _brewListFromSnapshot(QuerySnapshot snapshot) {
+    return snapshot.docs.map((doc) {
+      return Brew(
+          name: doc.get('name') ?? '',
+          strength: doc.get('strength') ?? 0,
+          sugar: doc.get('sugar') ?? '0');
+    }).toList();
+  }
+
+  //get brews stream
+  Stream<List<Brew>> get brews {
+    return brewCollection.snapshots().map(_brewListFromSnapshot);
+  }
+}
